1c92991fac4dc3c7c878867ab9370d9c9f22e2ae
burnworks <6436833+burnworks@users.noreply.github.com>
2025-08-15 18:11:47 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://burnworks.github.io',
+    base: '/burnworks-wcag-success-criteria-checklist',
+    trailingSlash: "always",
     vite: { plugins: [tailwindcss()], },
 });
